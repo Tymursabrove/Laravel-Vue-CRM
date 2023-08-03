84351ce56f25040285349076d20f917b2a122b14
Tymursabrove <tymursabrove0329@gmail.com>
2023-08-03 16:09:03 +0500

    Add VueRouter_to package.json update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,15 @@
-
 require('./bootstrap');
 
-import Vue from 'vue';
 
+import Vue from 'vue';
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
 Vue.component('home', require('./components/Home.vue').default);
 
 
+let router = new VueRouter({});
 const app = new Vue({
-    el: "#app"
+    el: "#app",
+    router
 });
